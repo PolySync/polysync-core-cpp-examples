3e60cde25032ff2389a1ceb0095472395f092bba
DM <dmiller@harbrick.com>
2015-11-15 14:16:06 -0800

    Object classification coloring.

--- ObjectWriter/ObjectWriter/main.cpp
+++ ObjectWriter/ObjectWriter/main.cpp
@@ -9,6 +9,7 @@ int main()
 {
     polysync::Node node;
     auto nodeRef = node.reference();
+    auto index = OBJECT_CLASSIFICATION_CAR;
     while( 1 )
     {
         // create a message
@@ -19,19 +20,41 @@ int main()
 
         // populate an object for the output buffer
         ps_object o;
-        o.classification = OBJECT_CLASSIFICATION_CAR;
-        o.classification_quality = QUALITY_9;
-        o.course_angle = 90.0;
-        o.id = 0;
-        o.position[0] = 5;
-        o.position[1] = 5;
-        o.position[2] = 5;
 
-        o.timestamp = 654645;
-        o.velocity[0] = 5;
-        o.velocity[1] = 5;
-        o.velocity[2] = 5;
+        // alternate data
+        if( index == OBJECT_CLASSIFICATION_CAR )
+        {
+            o.classification = index;
+            o.position[0] = 5;
+            o.position[1] = 5;
+            o.position[2] = 1;
+            o.id = 0;
 
+            o.course_angle = 90.0;
+            index = OBJECT_CLASSIFICATION_PEDESTRIAN;
+        }
+        else if( index == OBJECT_CLASSIFICATION_PEDESTRIAN )
+        {
+            o.classification = index;
+            o.position[0] = 5;
+            o.position[1] = -5;
+            o.position[2] = 1;
+            o.id = 0;
+
+            o.course_angle = -90.0;
+            index = OBJECT_CLASSIFICATION_BIKE;
+        }
+        else if( index == OBJECT_CLASSIFICATION_BIKE )
+        {
+            o.classification = index;
+            o.position[0] = 5;
+            o.position[1] = 0;
+            o.position[2] = 1;
+            o.id = 0;
+
+            o.course_angle = 0;
+            index = OBJECT_CLASSIFICATION_CAR;
+        }
         // place object to the object buffer in the message
         message.objectsBufferPushBack( o );
 
