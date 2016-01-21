04605216776095d53b1fd36d9a0b8097e403be9e
Lj <ljohnson@harbrick.com>
2016-01-21 15:18:15 -0800

    Base LidarReader class to filter pts by intesity
    Base taken from the Reader example application.
    
    Goal is to show basic usage of the various message types. This example is the first of many similar
    
    TODO - subclass the Reader class for each message-type filter application.

---
+++ LidarReader/LidarReader.hpp
@@ -0,0 +1,74 @@
+#include <map>
+#include <future>
+#include <memory>
+
+#include <PolySyncNode>
+#include <PolySyncMessage>
+#include <PolySyncDataModel>
+
+using namespace std;
+
+/**
+ * @brief The Reader class
+ * Subclass of polysync::Node. The base class handles registering to the bus.
+ * Dynamically gather type data. Register to types. Print data as it comes in.
+ *
+ * @note Run the Writer example if you need to make data availabe on bus.
+ */
+class Reader : public polysync::Node
+{
+public:
+    /**
+     * @brief LidarReader constructor
+     */
+    explicit                                Reader();
+
+    /**
+     * @brief exec Begins the Backend execution loop
+     */
+    void                                    execute();
+
+private:
+    /**
+     * @brief _sleepInterval Sleep between loop iterations.
+     */
+    static const uint                       _sleepInterval  { 100000 };
+
+    /**
+     * @brief _typeNameMap
+     */
+    map< string, polysync::MessageType  >   _typeNameMap;
+
+    /**
+     * @brief buildTypeMap
+     * Map MessageType values to string names in a std::map
+     */
+    void                                    buildTypeList();
+
+    /**
+     * @brief registerListeners
+     * Register polysync::Node::polysyncCallback() to receive ps_objects_msg,
+     * ps_radar_targets_msg, and ps_lidar_points_msg
+     */
+    void                                    registerListeners();
+
+    /**
+     * @brief printTypes
+     * List each registered message type in the PolySync Data Model.
+     */
+    void                                    printTypes();
+
+    /**
+     * @brief printMessage
+     * @param [in] Base class object pointer to cast and access data.
+     */
+    void                                    printMessage( polysync::Message * );
+
+    /**
+     * @brief filterLidarPoints
+     * Filter Lidar data for points of interest (to be defined later...).
+     */
+    template< typename T >
+    void                                    filterLidarPoints(
+                                                polysync::Message *message );
+};
---
+++ LidarReader/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "LidarReader.hpp"
+
+using namespace std;
+
+int main()
+{
+    Reader reader;
+
+    reader.execute();
+
+    return 0;
+}
+
+
