9c6b6d6bb23f89bb7b07a9fdbc4b09ab0413a515
Dave <dmiller@harbrick.com>
2016-02-28 14:57:39 -0800

    Placeholder.

--- DataGenerator/DataGenerator.cpp
+++ DataGenerator/DataGenerator.cpp
@@ -3,49 +3,27 @@
 #include <PolySyncNode.hpp>
 #include <PolySyncDataModel.hpp>
 
-using namespace std;
+#include "LidarPointGenerator.hpp"
+
+using namespace polysync::datamodel;
 
 class DataGenerator : public polysync::Node
 {
+protected:
+
     virtual void okStateEvent()
     {
-        polysync::datamodel::LidarPointsMessage lidarMessage( *this );
 
-        polysync::datamodel::SensorDescriptor descriptor;
-
-        descriptor.setId( 11 );
-        descriptor.setTransformParentId( PSYNC_COORDINATE_FRAME_LOCAL );
-        descriptor.setType( PSYNC_SENSOR_KIND_NOT_AVAILABLE );
-
-        lidarMessage.setSensorDescriptor( descriptor );
-
-        auto time = polysync::getTimestamp();
-        lidarMessage.setHeaderTimestamp( time );
-
-        for( auto pointNum = 0; pointNum < 10000; ++pointNum )
-        {
-            lidarMessage.setStartTimestamp( time );
-            lidarMessage.setEndTimestamp( time );
-
-            polysync::datamodel::LidarPoint point;
-            point.setIntensity( 255 );
-
-            auto x = pointNum % 100;
-            auto y = pointNum / 100;
-
-            double u = static_cast< double >( x )/ 100.0;
-            double v = static_cast< double >( y ) / 100.0;
-
-//            auto w = sin((u * 4.0) + global_relative_time) * cos((v * sine_freq) + global_relative_time) * 0.5;
-
-//            point.setPosition( { u*10, v*10, w*10 } );
-        }
     }
 
     virtual void initStateEvent()
     {
 
     }
+
+private:
+
+    LidarPointGenerator lidarPointGenerator;
 };
 
 int main(int argc, char *argv[])
