10a22835e910bef2fa763565baf5d0925e51d100
Amanda Falke <abstractmachines@gmail.com>
2016-05-11 16:02:21 -0700

    Psr 29 echo utility improvements (#8)
    * Psr 29 polysync echo command line tool (#7)
    
    * Echo.cpp one msg
    
    * Echo complete.
    
    * Improvements 1st PR, need getopt for opts/args.
    
    * Echo and getopt 100%. Need classes.
    
    * Modular classes GetOpt and Help
    
    * PSyncEchoHelp class header doxy
    
    * All suggested PR improvements completed.
    
    * argc/v descriptions
    
    * doxy
    
    * Echo naming conventions/improvements
    
    * Naming conventions
    
    * whitespace
    
    * sparser logic getopt
    
    * whitespace, public facing comments.
    
    * removing empty ctor
    
    * whitespace
    
    * header include style
    
    * header include styling

---
+++ Echo/ApplicationInputHandler.cpp
@@ -0,0 +1,184 @@
+#include <algorithm>
+#include <getopt.h>
+#include <PolySyncDataModel.hpp>
+
+#include "ApplicationInputHandler.hpp"
+
+using namespace std;
+
+namespace polysync
+{
+
+int ApplicationInputHandler::getFlagIndex( const char optret )
+{
+    for ( auto index = 0; index < _optionInputFlags.size(); ++index )
+    {
+        if( _optionInputFlags[ index ] == optret )
+        {
+            return index;
+        }
+    }
+    return -1;
+}
+
+
+
+bool ApplicationInputHandler::optionsParse( const int argc, char *argv[] )
+{
+    bool parsedOptSuccess = true;
+
+    int optionIndex = -1;
+
+    int optionArgumentIndex = 0;
+
+    // reset scanner
+    optind = 0;
+
+    int index;
+
+    opterr = 0;
+
+    while ( ( optionArgumentIndex = getopt( argc, argv, "o:f:hH") ) != -1 )
+    {
+        optionIndex = getFlagIndex( (const char) optionArgumentIndex );
+
+        if ( optionIndex == -1 )
+        {
+            cout << "\n\nUsage: invalid options. Usage guide follows." <<endl;
+
+            _getOptHelpFlag = true;
+        }
+
+        else if ( optionIndex != -1 )
+        {
+            switch ( optionArgumentIndex )
+            {
+
+            case 'f':
+
+                if ( ( *argv[ optind - 1 ] )
+                     && ( *argv[ optind - 1 ] == '-' ) )
+                {
+                    cout <<"\n\nInvalid usage for option -f filter single message type:\n"
+                         <<"-f should be followed by a PolySync message type,"
+                         << " not by another -option.\n"
+                         <<"A usage guide follows." << endl;
+
+                    _getOptHelpFlag = true;
+                }
+                else
+                {
+                    _messageName = optarg;
+
+                    _filteredForSingleMessageFlag = true;
+
+                }
+                break;
+
+            case 'o':
+
+                if ( ( *argv[ optind - 1 ] )
+                     && ( *argv[ optind - 1 ] == '-' ) )
+                {
+                    cout <<"\n\nInvalid usage for option -o external file:\n"
+                         <<"-o should be followed by a filename yourfile.txt,"
+                         << " not by another -option.\n"
+                         <<"A usage guide follows." << endl;
+
+                    _getOptHelpFlag = true;
+                }
+                else
+                {
+                    _userFileName = optarg;
+
+                    _echoMessageToFileFlag = true;
+                }
+                break;
+
+            case 'h':
+
+                _getOptHelpFlag = true;
+
+                break;
+
+            case 'H':
+
+                _echoMessageHeadersOnlyFlag = true;
+
+                break;
+
+            case '?':
+
+                if ( optopt == 'f' )
+                {
+                    _getOptHelpFlag = true;
+
+                    parsedOptSuccess = false;
+                }
+
+                else if ( optopt == 'o' )
+                {
+                    _getOptHelpFlag = true;
+
+                    parsedOptSuccess = false;
+                }
+
+            default:
+
+                parsedOptSuccess = true;
+            }
+        }
+    }
+
+    for ( index = optind; index < argc; ++index )
+    {
+        cout << "\n\nUsage: Non option argument: " << argv[ index ] <<endl
+             << "A usage guide follows." <<endl;
+
+        _getOptHelpFlag = true;
+    }
+
+    return parsedOptSuccess;
+}
+
+
+std::string ApplicationInputHandler::getMessageName() const
+{
+    return _messageName;
+}
+
+
+std::string ApplicationInputHandler::getFileName() const
+{
+    return _userFileName;
+}
+
+
+bool ApplicationInputHandler::singleMessageWasFiltered() const
+{
+    return _filteredForSingleMessageFlag;
+}
+
+
+bool ApplicationInputHandler::headersWereRequested() const
+{
+    return _echoMessageHeadersOnlyFlag;
+}
+
+
+bool ApplicationInputHandler::fileWasSpecified() const
+{
+    return _echoMessageToFileFlag;
+}
+
+
+bool ApplicationInputHandler::helpWasRequested() const
+{
+    return _getOptHelpFlag;
+}
+
+
+// END polysync::PolySyncGetOpt class
+
+
+} // end namespace polysync
---
+++ Echo/Echo.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2015 HARBRICK TECHNOLOGIES, INC
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+/**
+ * \example Echo.cpp
+ *
+ * PolySync Echo C++ API example application
+ * 
+ * Demonstrates a command line utility for echoing PolySync messges to stdout
+ *
+ * @file Echo.cpp
+ * @brief Echo Source
+ *
+ */
+
+#include "EchoNode.hpp"
+
+using namespace std;
+using namespace polysync;
+
+
+/**
+ * Entry point for this tutorial application
+ * The "connectPolySync" begins this node's PolySync execution loop.
+ *
+ * @param argc - int, the number of parameters on the command-line
+ * @param argv - char* [], the parsed command-line arguments
+ * 
+ * @return int - exit code
+ */
+int main( int argc, char *argv[] )
+{
+    // Create an instance of the PolySyncEcho and connect it to PolySync.
+    PolySyncEcho echo;
+
+    // Nodes will only connect if help option -h not used,
+    // and, if all arguments are valid.
+    if ( echo.optionsParse( argc, argv ) )
+    {
+        if ( echo.wasHelpRequested() )
+        {
+            echo.printEchoHelpPage();
+        }
+        else
+        {
+            // Begin the PolySync event loop.
+            echo.connectPolySync();
+        }
+    }
+
+    // For a brief tutorial on the Echo tool, run help: $ polysync-echo -h
+
+    return 0;
+}
