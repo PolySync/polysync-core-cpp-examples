f0d9635c2ea4c2fe7a15b990d1db6f1722a6c5c8
Lj <ljohnson@harbrick.com>
2016-11-14 19:51:06 -0800

    Add Daniel's path planner project
    Be sure to check out the tutorial from help.polysync.io

---
+++ PathPlanner/include/GridMap.hpp
@@ -0,0 +1,90 @@
+//GridMap.hpp
+#ifndef GRIDMAP_HPP
+#define GRIDMAP_HPP
+
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+using namespace cv;
+using namespace std;
+
+
+class GridMap
+{
+
+public:
+
+    GridMap( );
+
+    ~GridMap( );
+
+    void generateMap( );
+
+    void generateMap( int x, int y );
+
+    void moveRobot( int x, int y );
+
+    bool checkGoal( int index );
+
+    bool checkMove( int index, int size );
+
+    int getIndexFromState( int x, int y );
+
+    void getStateFromIndex( int index );
+
+
+public:
+
+    Mat map;
+
+    int nRows{ 500 };
+
+    int nCols{ 500 };
+
+    int robSize{ 29 };
+
+    int robLoc[4][2];
+
+    int golLoc[4][2];
+
+    int checkedMoveIndX;
+
+    int checkedMoveIndY;
+
+
+private:
+
+    void generateRobot( );
+
+    void generateGoal( );
+
+    void generateGoal( int x, int y );
+
+    void fillQuad(int ( &location )[ 4 ][ 2 ], int size);
+
+    void showMap( );
+
+    void updateMap( );
+
+    bool checkHit( int x, int y, int size );
+
+
+private:
+
+    Mat _robot;
+
+    Mat _gol;
+
+    Mat _staticMap;
+
+    Mat _pathMap;
+
+    string _mapID{ "resources/maze2.pgm" };
+
+    string _robID{ "resources/robot.jpg" };
+
+    string _golID{ "resources/gold.jpg" };
+
+};
+
+#endif // GRIDMAP_HPP
